12f72471ddc60aeb32080e94e7cad9064ada0ccd
Maria <mmorrison1978@gmail.com>
2016-03-06 15:56:18 -0700

    API routes wired up

--- API/routes/api.js
+++ API/routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
+
 function Learning() {
   return knex('learning');
 }
@@ -10,5 +11,17 @@ router.get('/', function(req, res, next) {
      res.json(learning);
   })
 });
+
+router.get('/:id', function(req, res, next) {
+  Learning().where({id: req.params.id}).then(function(results) {
+    res.json(results)
+  })
+});
+
+router.post('/', function(req, res, next) {
+  Learning().insert(req.body).then(function() {
+    res.json({success: true})
+  })
+})
 
 module.exports = router;
